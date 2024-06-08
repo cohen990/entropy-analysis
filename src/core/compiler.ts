c3212a5a8ca38ca045ea78736e333be7138c75ab
Samantha Cohen <samanthacohen294@gmail.com>
2024-06-08 01:14:34 +0100

    Cleanups and improvements

--- src/core/compiler.ts
+++ src/core/compiler.ts
@@ -1,8 +1,7 @@
 import * as ts from "typescript";
-import { inspect } from "util";
 import { EntropyNode, EntropyTreeRoot } from "./entropyTree";
 
-export function extract(file: string): EntropyNode {
+export const extract: (file: string) => EntropyNode = (file) => {
     const compilerOptions: ts.CompilerOptions = {
         allowJs: true,
         moduleResolution: ts.ModuleResolutionKind.NodeNext,
@@ -10,9 +9,6 @@ export function extract(file: string): EntropyNode {
     };
     let program = ts.createProgram([file], compilerOptions);
     const sourceFile = program.getSourceFile(file);
-    debug(`sourceFile: ${sourceFile}`);
-
-    info(`Processing ${sourceFile.text.split("\n").length} lines of code`);
 
     var nodeCount = 0;
 
@@ -22,41 +18,19 @@ export function extract(file: string): EntropyNode {
 
     var nodesToSearchBuffer: EntropyNode[] = [];
 
-    debug(`starting with ${inspect(nodesToSearch)}`);
-
     while (nodesToSearch.length > 0) {
-        debug(`scanning ${nodesToSearch.length} nodes`);
-
         for (var node of nodesToSearch) {
             if (!node) {
                 continue;
             }
 
-            debug(`scanning a ${node.syntaxKind()} node`);
-
             nodeCount++;
-
-            trace(inspect(node, undefined, 2));
-
             nodesToSearchBuffer.push(...node.getChildren());
         }
-
-        debug(`found ${nodesToSearchBuffer.length} more nodes`);
 
         nodesToSearch = [...nodesToSearchBuffer];
         nodesToSearchBuffer = [];
     }
 
-    info(`Discovered a total of ${nodeCount} nodes`);
     return entropyTreeRoot;
-}
-
-const info = (message: string) => {
-    // console.log(message);
 };
-
-const debug = (message: string) => {
-    // console.log(message);
-};
-
-const trace = (message: string) => null; //log(message);
