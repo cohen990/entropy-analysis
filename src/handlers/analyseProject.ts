fea9b7b7c1dd442ed8504a3b3053ceab783c49fa
Samantha Cohen <samanthacohen294@gmail.com>
2024-06-10 22:37:46 +0100

    implement workers

--- src/handlers/analyseProject.ts
+++ src/handlers/analyseProject.ts
@@ -1,10 +1,12 @@
-import { initialiseCache } from "../core/cache";
+import { fileHasher, initialiseCache } from "../core/cache";
 import { discoverFiles } from "../core/discoverFiles";
 import { computeEntropy } from "../core/entropy";
 import { sanitiseFileName } from "../core/fileNames";
-import { buildFileTree } from "../core/fileTree";
+import { AnalysisParameters, buildFileTree } from "../core/fileTree";
 import { bigintDeserialiser, bigintSerialiser } from "../core/serialiser";
+import { AnalyseWorkerData, AnalyseWorkerResults } from "../workers/analyse";
 import { AnalyseProjectArgs } from "./analyseProjectArgs";
+import { Worker } from "worker_threads";
 
 export const analyseProject: (
     args: AnalyseProjectArgs
@@ -20,13 +22,28 @@ export const analyseProject: (
     );
 
     const files = discoverFiles(path, ...(exclude || []));
-    console.log(`discovered ${files.length} typescript files`);
+    console.log(`discovered ${files.length} files`);
 
     const fileTree = buildFileTree(path, files);
 
-    const analysers = fileTree.getFileAnalysers(cache);
+    const analysisParameters = fileTree.getAnalysisParameters();
 
-    await Promise.all(analysers.map((x) => x()));
+    console.log(`about to create ${analysisParameters.length} workers`);
+
+    const promises: Promise<void>[] = analysisParameters.map(async (x) => {
+        const omega = await cache.cache(
+            analyseWithWorker(x),
+            x.fullFilePath.replace(x.rootPath, ""),
+            fileHasher(x.fullFilePath)
+        );
+
+        const nodePath = `${x.filePath.replace(`${x.rootPath}/`, "")}${
+            x.fileName
+        }`.split("/");
+        fileTree.setFileOmega(nodePath, omega);
+    });
+
+    await Promise.all(promises);
 
     const treeOmega = fileTree.recomputeTreeOmega();
 
@@ -35,3 +52,15 @@ export const analyseProject: (
     console.log(`project entropy calculated: ${treeEntropy}`);
     return treeEntropy;
 };
+const analyseWithWorker: (x: AnalysisParameters) => () => Promise<bigint> =
+    (x) => () =>
+        new Promise((resolve, reject) => {
+            const worker = new Worker("./src/workers/analyse.ts", {
+                workerData: x as AnalyseWorkerData,
+            });
+            worker.on("message", (data: AnalyseWorkerResults) => {
+                resolve(BigInt(data.omega));
+            });
+            worker.on("error", reject);
+            worker.on("exit", reject);
+        });
