178e16a41dc275fa0e5066a3f21e2dd3a721a323
Samantha Cohen <samanthacohen294@gmail.com>
2024-06-10 20:39:41 +0100

    implement caching

--- src/cli/countProjectLoc.ts
+++ src/cli/countProjectLoc.ts
@@ -5,6 +5,7 @@ import { AnalyseProjectArgs } from "../handlers/analyseProjectArgs";
 export const args = parse<AnalyseProjectArgs>({
     owner: { type: String, alias: "o", optional: true },
     repo: { type: String, alias: "r", optional: true },
+    ref: { type: String, alias: "f", optional: true },
     exclude: { type: String, alias: "x", optional: true, lazyMultiple: true },
 });
 
--- src/handlers/analyseProject.ts
+++ src/handlers/analyseProject.ts
@@ -1,3 +1,4 @@
+import { initializeOmegaCache } from "../core/cache";
 import { discoverFiles } from "../core/discoverFiles";
 import { computeEntropy } from "../core/entropy";
 import { sanitiseFileName } from "../core/fileNames";
@@ -6,15 +7,18 @@ import { AnalyseProjectArgs } from "./analyseProjectArgs";
 
 export const analyseProject: (
     args: AnalyseProjectArgs
-) => Promise<number> = async ({ owner, repo, exclude }) => {
-    const projectDirectory = sanitiseFileName(`${owner}-${repo}`);
-    const path = `${process.cwd()}/analysables/${projectDirectory}/`;
+) => Promise<number> = async ({ owner, repo, exclude, ref }) => {
+    const projectKey = sanitiseFileName(`${owner}-${repo}`);
+    const analysableKey = sanitiseFileName(`${projectKey}-${ref.slice(0, 7)}`);
+    const path = `${process.cwd()}/analysables/${projectKey}/${analysableKey}`;
+    const cache = initializeOmegaCache(projectKey);
+
     const files = discoverFiles(path, ...(exclude || []));
     console.log(`discovered ${files.length} typescript files`);
 
     const fileTree = buildFileTree(path, files);
 
-    const analysers = fileTree.getFileAnalysers();
+    const analysers = fileTree.getFileAnalysers(cache);
 
     await Promise.all(analysers.map((x) => x()));
 
