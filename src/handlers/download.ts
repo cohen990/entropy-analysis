e8d9eb115822de844226703e93aba2930db4c403
Samantha Cohen <samanthacohen294@gmail.com>
2024-06-10 20:50:02 +0100

    implement download caching

--- src/handlers/download.ts
+++ src/handlers/download.ts
@@ -2,6 +2,7 @@ import { Octokit } from "@octokit/rest";
 import { sanitiseFileName } from "../core/fileNames";
 import { mkdirSync, createWriteStream, existsSync } from "fs";
 import { Entry, fromBuffer } from "yauzl";
+import { initialiseCache } from "../core/cache";
 
 export interface DownloadArgs {
     owner: string;
@@ -19,7 +20,14 @@ export interface DownloadResult {
 export const download: (
     args: DownloadArgs
 ) => Promise<DownloadResult | undefined> = async ({ owner, repo }) => {
-    console.log(`Downloading ${owner}/${repo}`);
+    const project = `${owner}-${repo}`;
+    const cache = initialiseCache<DownloadResult>(
+        project,
+        "download",
+        JSON.stringify,
+        JSON.parse
+    );
+    console.log(`Downloading ${project}`);
 
     const octokit = new Octokit({
         auth: process.env["GITHUB_ACCESS_TOKEN"],
@@ -31,48 +39,56 @@ export const download: (
     });
     const ref = repoResponse.data.default_branch;
 
-    const languagesResponse = await octokit.rest.repos.listLanguages({
-        owner,
-        repo,
-    });
+    return await cache.cache(
+        async () => {
+            const languagesResponse = await octokit.rest.repos.listLanguages({
+                owner,
+                repo,
+            });
 
-    const commitResponse = await octokit.rest.repos.getCommit({
-        owner,
-        repo,
-        ref,
-    });
+            const commitResponse = await octokit.rest.repos.getCommit({
+                owner,
+                repo,
+                ref,
+            });
 
-    const zipResponse = await octokit.rest.repos.downloadZipballArchive({
-        owner,
-        repo,
-        ref,
-    });
+            const zipResponse = await octokit.rest.repos.downloadZipballArchive(
+                {
+                    owner,
+                    repo,
+                    ref,
+                }
+            );
 
-    const analysablesRoot = `${process.cwd()}/analysables`;
+            const analysablesRoot = `${process.cwd()}/analysables`;
 
-    if (!existsSync(analysablesRoot)) {
-        mkdirSync(analysablesRoot);
-    }
+            if (!existsSync(analysablesRoot)) {
+                mkdirSync(analysablesRoot);
+            }
 
-    const targetDirectory =
-        analysablesRoot + "/" + sanitiseFileName(`${owner}-${repo}`);
-    if (!existsSync(targetDirectory)) {
-        mkdirSync(targetDirectory);
-    }
+            const targetDirectory =
+                analysablesRoot + "/" + sanitiseFileName(`${owner}-${repo}`);
+            if (!existsSync(targetDirectory)) {
+                mkdirSync(targetDirectory);
+            }
 
-    const buffer = Buffer.from(zipResponse.data as ArrayBuffer);
+            const buffer = Buffer.from(zipResponse.data as ArrayBuffer);
 
-    const unzipSize = await unzip(buffer, targetDirectory);
+            const unzipSize = await unzip(buffer, targetDirectory);
 
-    const result: DownloadResult = {
-        sizeInBytes: unzipSize,
-        openIssuesCount: repoResponse.data.open_issues_count,
-        allLanguages: languagesResponse.data,
-        primaryLanguage: Object.keys(languagesResponse.data)[0],
-        refSha: commitResponse.data.sha,
-    };
+            const result: DownloadResult = {
+                sizeInBytes: unzipSize,
+                openIssuesCount: repoResponse.data.open_issues_count,
+                allLanguages: languagesResponse.data,
+                primaryLanguage: Object.keys(languagesResponse.data)[0],
+                refSha: commitResponse.data.sha,
+            };
 
-    return result;
+            return result;
+        },
+        project,
+        ref
+    );
 };
 
 const unzip: (buffer: Buffer, path: string) => Promise<number> = (
