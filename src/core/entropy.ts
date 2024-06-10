98031feb30cfff4ae3f49dfa7cdaeffc8f682769
Sammie Cohen <samantha.cohen@ovo.com>
2024-06-10 16:11:32 +0100

    fix the constructor problem! for those weird libs that are trying too hard

--- src/core/entropy.ts
+++ src/core/entropy.ts
@@ -10,10 +10,11 @@ const naturalLog: (input: bigint) => number = (input) => {
 
     return (
         Math.log(16) *
-            (inputAsString.length - fifteenSignificantFigures.length) +
+        (inputAsString.length - fifteenSignificantFigures.length) +
         Math.log(("0x" + fifteenSignificantFigures) as any)
     );
 };
+
 export const factorial: (number: number) => bigint = (number) => {
     var result = BigInt(1);
     for (var i = 2; i <= number; i++) {
