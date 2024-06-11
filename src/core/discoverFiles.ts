5f2073b2b28d6907317d7826648515d6b0fe2b71
Samantha Cohen <samanthacohen294@gmail.com>
2024-06-11 10:56:54 +0100

    make path correction work for organisations where the sha is not clipped to 7 chars

--- src/core/discoverFiles.ts
+++ src/core/discoverFiles.ts
@@ -5,7 +5,7 @@ export const discoverFiles: (path: string, ...filters: string[]) => string[] = (
     path,
     ...filters: string[]
 ) => {
-    var result = sync(path + "/**/*.{j,t}s?(x)");
+    var result = sync(path + "/**/*.{j,t}s");
     for (var filter of filters) {
         result = result.filter((x) => !minimatch(x, filter));
     }
--- src/core/fileSystem.ts
+++ src/core/fileSystem.ts
@@ -4,4 +4,4 @@ export const sanitiseFileName: (input: string) => string = (input: string) =>
     input.replace(".", "-").toLowerCase();
 
 export const correctDirectoryCasing = (guessedPath: string) =>
-    sync(guessedPath, { nocase: true })[0];
+    sync(`${guessedPath}*`, { nocase: true })[0];
